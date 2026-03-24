ec4f8283b5ff5de247071279dfde36851a2771ff
DevGangMin <fire0609@naver.com>
2026-03-24 11:05:51 +0900

    feat: Complete TodoForm and creation logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import "./App.module.css";
+import { useState } from "react";
+import styles from "./App.module.css";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   // TODO: State 관리 용 로직
+  const [todos, setTodos] = useState([]);
 
   // TODO: Create
   // 텍스트 입력창과 "추가" 버튼이 있어야 합니다
@@ -9,20 +13,32 @@ function App() {
   // 추가 후 입력창은 비워져야 합니다
   // 빈 문자열은 추가할 수 없어야 합니다
 
+  const handleAddTodo = (title) => {
+    const newTodo = {
+      id: Date.now(),
+      title: title,
+      completed: false,
+      createdAt: new Date(),
+    };
+    setTodos([...todos, newTodo]);
+  };
+
   // TODO: Update (todos.map 사용)
   // 할 일 목록의 항목에는 "완료" 버튼이 있어야 합니다
   // "완료" 버튼을 누르면 해당 항목이 완료 목록으로 이동합니다
   // 완료 목록의 항목에는 "취소" 버튼이 있어야 합니다
   // "취소" 버튼을 누르면 해당 항목이 다시 할일 목록으로 이동합니다
 
+
   // TODO: Delete (todos.filter 사용 - 해당 id를 제외한 새 배열을 set)
   // 각 할일에 "삭제" 버튼이 있어야 합니다
   // "삭제" 버튼을 누르면 해당 할일이 목록에서 제거됩니다
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Todo List</h1>
       {/* TODO: TodoForm 컴포넌트 배치 */}
       {/* - TodoForm 컴포넌트에 할 일 추가 함수를 props로 전달하여 상태를 변경 */}
+      <TodoForm onAddTodo={handleAddTodo} />
 
       {/* TODO: 할 일 목록 */}
       {/* - 제목 : <h2>할 일 목록</h2>
@@ -30,11 +46,13 @@ function App() {
       - todos.filter를 써서 completed가 false 인 것만 추출하여 TodoList 컴포넌트로 전달
       - props로 '완료/취소 함수' 와 '삭제 함수' 전달 */}
 
+
       {/* TODO: 완료 목록 */}
       {/* - 제목 : <h2>완료 목록</h2>
       - 생성 시간
       - todos.filter를 써서 completed가 true 인 것만 추출하여 TodoList 컴포넌트로 전달
       - props로 '완료/취소 함수' 와 '삭제 함수' 전달 */}
+
     </div>
   );
 }
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,2 +1,28 @@
-// TODO: 할 일 입력 폼
-// 상태(`todos`)는 `App.jsx`에서 관리합니다
+import React, { useState } from "react";
+
+function TodoForm({ onAddTodo }) {
+  const [text, setText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() === "") {
+      alert("할 일을 입력해주세요!");
+      return;
+    }
+    onAddTodo(text);
+
+    setText("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="할 일을 입력하세요."
+      />
+      <button type="submit">추가</button>
+    </form>
+  );
+}
+
+export default TodoForm;
